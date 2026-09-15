b0cf31ed016eb22e41d698809f89193335714656
yamival-blip <yamival@gmail.com>
2026-09-15 18:58:24 -0300

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,77 @@
+const CACHE = "ng-licitaciones-v1";
+
+const ASSETS = [
+
+  "/",
+
+  "/manifest.webmanifest"
+
+];
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+
+  );
+
+  self.skipWaiting();
+
+});
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+
+    caches.keys().then(keys =>
+
+      Promise.all(
+
+        keys
+
+          .filter(key => key !== CACHE)
+
+          .map(key => caches.delete(key))
+
+      )
+
+    )
+
+  );
+
+  self.clients.claim();
+
+});
+
+self.addEventListener("fetch", event => {
+
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+
+    fetch(event.request)
+
+      .then(response => {
+
+        const copy = response.clone();
+
+        caches.open(CACHE).then(cache => {
+
+          cache.put(event.request, copy);
+
+        });
+
+        return response;
+
+      })
+
+      .catch(() =>
+
+        caches.match(event.request)
+
+      )
+
+  );
+
+});
